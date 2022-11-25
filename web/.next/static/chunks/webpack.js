df86832fa46d149e9955f41b9d1f5ea4c5a409f2
dbrewfuk <dbrewfukuhara@gmail.com>
2022-11-24 20:57:27 -0500

    new commit

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -88,18 +88,6 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	!function() {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = function(module) {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				function() { return module['default']; } :
-/******/ 				function() { return module; };
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
@@ -128,7 +116,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a904af374476f7f"; }
+/******/ 		__webpack_require__.h = function() { return "f6c46846f0e7e1f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
